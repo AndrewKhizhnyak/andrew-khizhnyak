2fa3b8038d8accd01c5da9d7acaf9d5032f2cf73
andrew <andrew_khizhnyak@mail.ru>
2019-03-27 00:32:35 +0300

    Updates

---
+++ precache-manifest.e0e9a8248d3b02b382db4ecbf694398a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2c795436d9c15ce220f",
+    "url": "/static/js/main.f8d07d12.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "e2c795436d9c15ce220f",
+    "url": "/static/css/main.6b4b4776.chunk.css"
+  },
+  {
+    "revision": "be2b296f11160b5ac402c805c79070fc",
+    "url": "/index.html"
+  }
+];
