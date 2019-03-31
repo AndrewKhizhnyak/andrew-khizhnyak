795003a38b61b9cec61f4ffb7490b5d0518f8544
andrew <andrew_khizhnyak@mail.ru>
2019-03-31 23:54:07 +0300

    0.1.0

---
+++ src/components/AK/AK.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './AK.sass';
+
+export default class AK extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Andrew Khizhnyak
+        </h1>
+      </div>
+    )
+  }
+}
